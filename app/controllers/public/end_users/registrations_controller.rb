7d6b461205cfd324447bf5efa5dd5a70ec4f1bc3
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-06-04 02:51:44 +0000

    [Update]新規登録フォーム

--- app/controllers/public/end_users/registrations_controller.rb
+++ app/controllers/public/end_users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::EndUsers::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +60,12 @@ class Public::EndUsers::RegistrationsController < Devise::RegistrationsControlle
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_first_name, :kana_last_name])
+  end
+
+
 end
