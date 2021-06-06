a95d8c0f638edbf38a4cfc630a5c9304b387cc64
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-06-06 01:06:50 +0000

    [Add]models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :genre
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :foods
+
+
+  def favorited_by?(end_user)
+    favorites.where(end_user_id: end_user.id).exists?
+  end
+
 end
