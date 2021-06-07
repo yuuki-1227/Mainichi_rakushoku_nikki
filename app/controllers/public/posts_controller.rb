ff1b82f5bbc0002dea7cc056171c8f9b941d3a78
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-06-07 06:26:18 +0000

    [Update]posts,end_users,genres_function

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -26,13 +26,13 @@ class Public::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to end_user_path(@post)
+    redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to end_user_path(@post)
+    redirect_to post_path(@post)
   end
 
   private
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
 
   has_many :posts
-  
+
 end
