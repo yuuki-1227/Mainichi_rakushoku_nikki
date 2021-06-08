1448e93a442e19171fda79c1f8bf4f9aea5e17e7
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-06-08 08:20:50 +0000

    [Update]favorite_function

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,12 +4,14 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_end_user.favorites.build(post_id: params[:post_id])
     favorite.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(post_id: params[:post_id], end_user_id: current_end_user.id)
     favorite.destroy
+    redirect_back(fallback_location: root_path)
   end
 
 end
