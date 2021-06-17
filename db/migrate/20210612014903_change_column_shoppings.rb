c4666b3d7fba46601babbf871664ab2672a1d67d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-06-17 11:22:56 +0000

    migration_update

--- db/migrate/20210612014903_change_column_shoppings.rb
+++ db/migrate/20210612014903_change_column_shoppings.rb
@@ -1,5 +1,5 @@
 class ChangeColumnShoppings < ActiveRecord::Migration[5.2]
   def change
-    change_column :shoppings, :buy_date, :start_time
+    change_column :shoppings, :start_time, :buy_date
   end
 end
