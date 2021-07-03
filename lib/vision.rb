1c7e6a8bc499dda623b564929aba5e5e8bfcb5b8
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-07-03 02:09:21 +0000

    [Update]vision.rb

--- lib/vision.rb
+++ lib/vision.rb
@@ -30,14 +30,14 @@ module Vision
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       request = Net::HTTP::Post.new(uri.request_uri)
-      request['Content-Type'] = 'application/json'
+      request["Content-Type"] = 'application/json'
       response = https.request(request, params)
       response_body = JSON.parse(response.body)
       # APIレスポンス出力
-      if (error = response_body['responses'][0]['error']).present?
-        raise error['message']
+      if (error = response_body["responses"][0]["error"]).present?
+        raise error["message"]
       else
-        response_body['responses'][0]['labelAnnotations'].pluck('description').take(3)
+        response_body["responses"][0]["labelAnnotations"].pluck('description').take(3)
       end
     end
   end
