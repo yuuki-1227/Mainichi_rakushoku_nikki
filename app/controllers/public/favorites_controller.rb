8052446dd6e150a0646e142a627cc0cb37422a06
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-07-25 09:24:38 +0000

    [comment]favorites_controller

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,6 +2,8 @@ class Public::FavoritesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    # end_user(1)とfavorite(多)が関連づけられているため、buildを使用する
+    # buildを使用することによってend_userに紐づけられたfavoriteインスタンスを生成することが
     favorite = current_end_user.favorites.build(post_id: params[:post_id])
     favorite.save
   end
