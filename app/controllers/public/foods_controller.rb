863f11669bca03db70372fb2a159dffb9204afb2
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-09-07 01:51:56 +0000

    [Comment]foods_controller

--- app/controllers/public/foods_controller.rb
+++ app/controllers/public/foods_controller.rb
@@ -1,13 +1,16 @@
 class Public::FoodsController < ApplicationController
 
+  # 一覧画面
   def index
     @foods = Food.all
   end
 
+  # 詳細画面
   def show
     @food = Food.find(params[:id])
   end
 
+  # 新規作成 及び 登録
   def create
     @food = Food.new(food_params)
     @shopping = Shopping.find(params[:shopping_id])
@@ -16,16 +19,19 @@ class Public::FoodsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  # 編集画面
   def edit
     @food = Food.find(params[:id])
   end
 
+  # 更新
   def update
     @food = Food.find(params[:id])
     @food.update(food_params)
     redirect_to end_user_shopping_path(current_end_user, @food.shopping_id)
   end
 
+  # 削除
   def destroy
     @food = Food.find(params[:id])
     @food.destroy
