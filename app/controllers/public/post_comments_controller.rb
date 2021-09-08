958adfebc942fa3e34ef520636d4810fc7cc8ed0
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-09-08 10:23:11 +0000

    [Comment]post_comment_controller

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostCommentsController < ApplicationController
 
-  # 新規投稿登録
+  # 新規投稿コメント登録
   def create
     @post = Post.find(params[:post_id])
     @post_comment = current_end_user.post_comments.new(post_comment_params)
@@ -15,7 +15,7 @@ class Public::PostCommentsController < ApplicationController
     end
   end
 
-  # 既存投稿
+  # 既存投稿コメント削除
   def destroy
     @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
